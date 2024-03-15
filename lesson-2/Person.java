813ae722371e964161320480ab168cc8c512d884
Anastasia <anastasiavlg@mail.ru>
2024-03-15 17:45:05 +0300

    Write Person class

---
+++ lesson-2/Person.java
@@ -0,0 +1,27 @@
+public class Person {
+    char sex = 'm';
+    String name = "Иванов Иван Иванович";
+    float height = 1.82f;
+    double weight = 76.3;
+    int age = 34;
+
+    void go() {
+        System.out.println("Идёт");
+    }
+
+    void seat() {
+        System.out.println("Сидит");
+    }
+
+    void run() {
+        System.out.println("Бежит");
+    }
+
+    void speak() {
+        System.out.println("Говорит");
+    }
+
+    void learnJava() {
+        System.out.println("Учит Java");
+    }
+}
