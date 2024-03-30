7c5166a85d1f8efbd628f3b76143febad54c2456
Anastasia <anastasiavlg@mail.ru>
2024-03-30 23:45:19 +0300

    Make 'Guess Number' game interactive

--- lesson-2/GuessNumber.java
+++ lesson-2/GuessNumber.java
@@ -1,16 +1,38 @@
+import java.util.Random;
+import java.util.Scanner;
+
 public class GuessNumber {
-    public static void main(String[] args) {
-        int computerNumber = 40;
-        int playerNumber = 27;
+    private final Player firstPlayer;
+    private final Player secondPlayer;
+    private final Scanner scanner;
+    private final Random random;
+
+    public GuessNumber(Player firstPlayer, Player secondPlayer, Scanner scanner) {
+        this.firstPlayer = firstPlayer;
+        this.secondPlayer = secondPlayer;
+        this.scanner = scanner;
+        this.random = new Random();
+    }
+
+    public void play() {
+        int computerNumber = random.nextInt(1, 101);
+        Player currentPlayer = null;
+        int playerNumber;
         do {
+            System.out.println();
+            currentPlayer = currentPlayer == firstPlayer ? secondPlayer : firstPlayer;
+            playerNumber = askNumber(currentPlayer);
             if (playerNumber < computerNumber) {
                 System.out.printf("Число %d меньше того, что загадал компьютер%n", playerNumber);
-                playerNumber += 3;
             } else if (playerNumber > computerNumber) {
                 System.out.printf("Число %d больше того, что загадал компьютер%n", playerNumber);
-                playerNumber -= 2;
             }
         } while (playerNumber != computerNumber);
-        System.out.println("Вы победили!");
+        System.out.printf("%s, поздравляю, вы победили!%n%n", currentPlayer.getName());
+    }
+
+    private int askNumber(Player player) {
+        System.out.printf("%s, ваш ход: ", player.getName());
+        return scanner.nextInt();
     }
 }
---
+++ lesson-2/GuessNumberTest.java
@@ -0,0 +1,37 @@
+import java.util.Scanner;
+
+public class GuessNumberTest {
+    private final Scanner scanner = new Scanner(System.in);
+    private final GuessNumber guessNumber;
+
+    public GuessNumberTest() {
+        System.out.println("Представьтесь, пожалуйста:");
+        Player firstPlayer = askName(1);
+        Player secondPlayer = askName(2);
+        guessNumber = new GuessNumber(firstPlayer, secondPlayer, scanner);
+    }
+
+    private Player askName(int playerNumber) {
+        System.out.print(playerNumber + ". ");
+        return new Player(scanner.nextLine());
+    }
+
+    public void run() {
+        do {
+            guessNumber.play();
+        } while ("yes".equals(askToContinue()));
+    }
+
+    private String askToContinue() {
+        String answer;
+        do {
+            System.out.print("Хотите продолжить игру? [yes/no]: ");
+            answer = scanner.next().toLowerCase();
+        } while (!("yes".equals(answer) || "no".equals(answer)));
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        new GuessNumberTest().run();
+    }
+}
