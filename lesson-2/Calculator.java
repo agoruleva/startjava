8e04129ae564dc7f5ac3e5df8b59c55b073286e9
Anastasia <anastasiavlg@mail.ru>
2024-04-02 16:22:03 +0300

    Add forgotten '%' and '^' operation to Calculator class

--- lesson-2/Calculator.java
+++ lesson-2/Calculator.java
@@ -4,8 +4,18 @@ public class Calculator {
             case '+' -> a + b;
             case '-' -> a - b;
             case '*' -> a * b;
+            case '^' -> pow(a, b);
             case '/' -> a / b;
+            case '%' -> a % b;
             default -> throw new IllegalArgumentException("Неизвестная операция");
         };
+    }
+
+    private static int pow(int a, int b) {
+        int result = 1;
+        for (int i = 1; i <= b; ++i) {
+            result *= a;
+        }
+        return result;
     }
 }
